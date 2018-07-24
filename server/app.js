d7de2d78f629796f80f60e4dc82cce5d078de409
Ayelegun Kayode Michael <billmike1994@gmail.com>
2018-07-24 16:34:56 +0100

    chore(set-up-database): setup dabase connection
    - setup database connection
    - implement data schema
    
    [Finishes #159271450]

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dbKey from './config/config';
 
 const app = express();
+const mongoDB = process.env.MONGODB_URI || dbKey.databaseURL;
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
 
 const PORT = 5000;
+
+const db = mongoose.connection;
+db.on('error',
+  console.error.bind(console, 'MONGODB connection failed in this instance'));
+
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
---
+++ server/models/patient.model.js
@@ -0,0 +1,61 @@
+import { Schema } from 'mongoose';
+
+const PatientSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    max: 50
+  },
+  lastName: {
+    type: String,
+    required: true,
+    max: 50
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  occupation: {
+    type: String,
+    required: true
+  },
+  placeOfWork: {
+    type: String
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  maritalStatus: {
+    type: true,
+    required: true
+  },
+  nextOfKin: {
+    type: String,
+    required: true
+  },
+  nextOfKinAddress: {
+    type: String,
+    required: true,
+  },
+  relationship: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  nextOfKinNumber: {
+    type: Number,
+    required: true
+  },
+  photograph: {
+    type: String,
+    required: true
+  }
+});
